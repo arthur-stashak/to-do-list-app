1a88032f954d6c246f4158260d8bf8af76efdc52
arthur-stashak <arthur.stashak@outlook.com>
2022-02-23 17:41:24 +0100

    added toggleClass and .text

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,7 @@ function newItem(){
 //1. Adding a new item to the list of items:
   let li = $('<li></li>');
   let inputValue = $('#input').val();
-  let text = $(inputValue);
-  li.append(text);
+  li.append(inputValue);
 
   if (inputValue === '') {
     alert("You must write something!");
@@ -16,23 +15,21 @@ function newItem(){
 
  //2. Crossing out an item from the list of items:
   function crossOut() {
-    li.addClass.toggle('strike');
+    li.toggleClass('strike');
   }
 
-    li.on('dbclick', function() {
-    li.addClass("strike");
-  });
+  li.on('dblclick', crossOut);
 
  //3(i). Adding the delete button "X":
   let crossOutButton = $('<crossOutButton></crossOutButton>');
-  crossOutButton.append($('X'));
+  crossOutButton.text('X');
   li.append(crossOutButton);
 
   crossOutButton.on('click', deleteListItem);
 
  //3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
   function deleteListItem(){
-    li.addClass.add('delete')
+    li.addClass('delete');
   }
  // 4. Reordering the items:
    $('#list').sortable();
